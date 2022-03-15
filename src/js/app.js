2b8a4492095d94e6bb18647e3a48ad42e2535fe0
Magdalena Sojka <sojka.magdalena.90@gmail.com>
2022-03-15 20:28:03 +0100

    Add class Booking

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
 
@@ -104,6 +105,15 @@ const app = {
       app.cart.add(event.detail.product);
     });
   },
+
+  initBooking: function(){
+    const thisApp = this;
+
+    const bookingWidget = document.querySelector(select.containerOf.booking);
+    thisApp.Booking = new Booking(bookingWidget);
+  },
+
+
   init: function(){
     const thisApp = this;
     //console.log('*** App starting ***');
@@ -114,6 +124,7 @@ const app = {
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
   
   
